b59cb9ca31d5adb590c5d42cd712f8e9f4d2046c
matthewfen <mfenech58@gmail.com>
2021-01-03 20:01:33 +0100

    Jumptopia
    updated

---
+++ Jumptopia1/Assets/floor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float destroyTime = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(gameObject, destroyTime);
+    }
+}
---
+++ Jumptopia1/Assets/spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float maxTime = 1;
+private float timer = 0;
+public GameObject pipe;
+public float height;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > maxTime){
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = transform.position + new Vector3(Random.Range(-height,height) ,0,0);
+            timer = 0;
+        }
+    timer += Time.deltaTime;
+    }
+}
