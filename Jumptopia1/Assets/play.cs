84a887010e19e24d478adc40f552db33181463d5
nigel-borg <74591124+nigel-borg@users.noreply.github.com>
2021-01-12 20:48:36 +0100

    Jumptopia updates 3.0
    Multiple updates such as back icon
    scripts for play and levels increased the font size of jumptopia including level 2 and 3

---
+++ Jumptopia1/Assets/play.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class play : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void PLayame ()
+    {
+        //when you click on the play button you will see levels
+    
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
