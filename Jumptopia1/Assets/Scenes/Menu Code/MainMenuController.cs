009a812db49c1c3f34e76f875acdec10edadd2ef
nigel-borg <74591124+nigel-borg@users.noreply.github.com>
2021-01-17 19:29:32 +0100

    Jumptopia 5.0
    added character sprites to both level 1 and 2 made sperated main menu and levels 1 and 2

---
+++ Jumptopia1/Assets/Scenes/Menu Code/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+    public void Level2Game()
+    {
+        SceneManager.LoadScene("Level 2");
+    }
+
+
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+}
